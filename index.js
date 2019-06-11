22cfc0fe54d9f328d427d9a36b7aa4161e240ff0
Derek Miranda <derek.miranda@ff0000.com>
2019-06-10 17:39:38 -0700

    Adds sprite

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-let app = new PIXI.Application({ width: 256, height: 256 });
+// aliases
+let Application = PIXI.Application,
+  loader = PIXI.loader,
+  resources = PIXI.loader.resources,
+  Sprite = PIXI.Sprite;
 
-// auto-resize
-app.renderer.autoResize = true;
+let app = new Application({
+  width: 256,
+  height: 256,
+  antialias: true,
+  transparent: false,
+  resolution: 1
+});
 
-// make canvas fill entire window
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.resize(window.innerWidth, window.innerHeight);
+function initApp() {
+  // auto-resize
+  app.renderer.autoResize = true;
 
-document.body.appendChild(app.view);
+  // make canvas fill entire window
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(app.view);
+
+  load();
+}
+
+// this function loads images into the texture cache
+function load() {
+  loader.add("images/pixelme.png").load(setup);
+}
+
+function setup() {
+  let sprite = new Sprite(loader.resources["images/pixelme.png"].texture);
+
+  // add sprite to stage
+  app.stage.addChild(sprite);
+
+  // to remove sprite
+  // app.stage.removeChild(sprite)
+  // OR
+  // sprite.visible = false
+}
+
+initApp();
